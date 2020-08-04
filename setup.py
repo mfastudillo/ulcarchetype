7ffd1cc6af6136b084581134dd7ae4b54419fcdd
Miguel <miguelfastudillo@gmail.com>
2020-08-04 08:28:05 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(name="ulcarchetype",
+      version=0.1,
+      author="Miguel F. Astudillo",
+      author_email="migferast@gmail.com",
+      packages=setuptools.find_packages(),
+      license="LICENSE.txt",
+      description="package to estimate uncertainty related to lca archetypes",
+      long_description=open("README.md").read(),
+      install_requires=["pandas","brightway2",'numpy'],
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    ) 
